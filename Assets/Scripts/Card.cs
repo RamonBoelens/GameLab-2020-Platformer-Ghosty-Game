8c3d80d7d9f804980e534e1755ded0768305e8e4
RamonBoelens <ramonboelens@hotmail.com>
2020-03-03 23:17:40 +0100

    Update 0.1
    + Added Card Template
    + Added 5 Test Cards
    + Added Card Display
    + Made all the cards change values based upon the data in their sriptable object.
    
    - Deleted 'Slippery' physics material from previous project.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Category
+{
+    Cat1,
+    Cat2,
+    Cat3,
+    Cat4,
+    Cat5,
+}
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public Category category;
+    public string description;
+    public Sprite artwork;
+
+    public int GetCategoryNumber()
+    {
+        if (category == Category.Cat1)
+            return 1;
+        else if (category == Category.Cat2)
+            return 2;
+        else if (category == Category.Cat3)
+            return 3;
+        else if (category == Category.Cat4)
+            return 4;
+        else if (category == Category.Cat5)
+            return 5;
+        else
+            return 0;
+    }
+}
---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardDisplay : MonoBehaviour
+{
+
+    public Card card;
+
+    public TextMeshProUGUI categoryText;
+
+    public Image artworkImage;
+
+    public TextMeshProUGUI descriptionText;
+    public TextMeshProUGUI catergoryNumberText;
+
+    public Image cardTemplateImage;
+
+    private int category;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        category = card.GetCategoryNumber();
+
+        InitCardValues();
+        SetupCardColor();
+    }
+
+    private void InitCardValues()
+    {
+        categoryText.text = card.category.ToString();
+
+        artworkImage.sprite = card.artwork;
+
+        descriptionText.text = card.description;
+        catergoryNumberText.text = category.ToString();
+    }
+
+    private void SetupCardColor()
+    {
+        if (category == 0)
+        {
+            Debug.LogWarning("Card category isn't initialized.");
+            return;
+        }
+        else if (category == 1)
+        {
+            cardTemplateImage.color = Color.blue;
+        }
+        else if (category == 2)
+        {
+            cardTemplateImage.color = Color.cyan;
+        }
+        else if (category == 3)
+        {
+            cardTemplateImage.color = Color.green;
+        }
+        else if (category == 4)
+        {
+            cardTemplateImage.color = Color.magenta;
+        }
+        else if (category == 5)
+        {
+            cardTemplateImage.color = Color.yellow;
+        }
+    }
+}
