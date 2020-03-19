c5d253ecf8fb485dcb66157c1812cf16a93e28c9
RamonBoelens <ramonboelens@hotmail.com>
2020-03-19 19:24:27 +0100

    Stores Multiple Names
    + Added a list that stores the names

--- Assets/Scripts/NameTransfer.cs
+++ Assets/Scripts/NameTransfer.cs
@@ -2,19 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class NameTransfer : MonoBehaviour
 {
-    public  string theName;
-    public  GameObject inputField;
-    public  GameObject textDisplay;
+    private List<string> players = new List<string>();
+    private string theName;
 
-    public void StoreName() {
-        theName = inputField.GetComponent<Text>().text;
-        
-        textDisplay.GetComponent<Text>().text = theName;
+    public Text inputField;
+    public Text textDisplay;
 
+    private void Start()
+    {
+        UpdatePlayerDisplay();
     }
 
+    public void StoreName()
+    {
+        // Check if the field is empty
+            // If empty -> return;
 
+        // Check if the first or last character is a space
+            // If so delete those
+
+        // Check if the name already is in the list
+            // If so -> return
+
+        // Store the name in a list
+        theName = inputField.text;
+        players.Add(theName);
+
+        // Update the list of players on the screen
+        UpdatePlayerDisplay();
+    }
+
+    public void UpdatePlayerDisplay()
+    {
+        // If there are no players yet then empty the list
+        if (players.Count <= 0)
+        {
+            textDisplay.text = null;
+            return;
+        }
+
+        textDisplay.text = theName;
+    }
 }
