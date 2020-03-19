3486befcfdd4c5fea2d2a214b9670df164f4dc87
RamonBoelens <ramonboelens@hotmail.com>
2020-03-19 19:40:01 +0100

    Player Names
    Changed a few things for the player names

--- Assets/Scripts/NameTransfer.cs
+++ Assets/Scripts/NameTransfer.cs
@@ -6,31 +6,44 @@ using TMPro;
 
 public class NameTransfer : MonoBehaviour
 {
-    private List<string> players = new List<string>();
+    private List<string> playerNames = new List<string>();
     private string theName;
 
     public Text inputField;
     public Text textDisplay;
 
+    // Being able to change the char limit from this script
+
     private void Start()
     {
+        // Set the char limit
+
         UpdatePlayerDisplay();
     }
 
     public void StoreName()
     {
-        // Check if the field is empty
-            // If empty -> return;
-
         // Check if the first or last character is a space
             // If so delete those
+
+        // Check if there are double spaces in the string itself
+            // If so delete one of the double spaces
 
         // Check if the name already is in the list
             // If so -> return
 
+        // Check if the field is empty
+        if (inputField.text == "")
+        {
+            Debug.LogWarning("The inputfield was empty, so I couldn't add the player name.");
+            return;
+        }
+
         // Store the name in a list
         theName = inputField.text;
-        players.Add(theName);
+        playerNames.Add(theName);
+
+        // Reset the name in the input field
 
         // Update the list of players on the screen
         UpdatePlayerDisplay();
@@ -39,12 +52,16 @@ public class NameTransfer : MonoBehaviour
     public void UpdatePlayerDisplay()
     {
         // If there are no players yet then empty the list
-        if (players.Count <= 0)
+        if (playerNames.Count <= 0)
         {
             textDisplay.text = null;
             return;
         }
 
+        // Update the whole player list on the screen
+        // For now it just shows the latest added name
         textDisplay.text = theName;
     }
+
+    // When done send the whole list with player names to the GameManager
 }
