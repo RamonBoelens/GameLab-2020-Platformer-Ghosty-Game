d389a287ba982fd7366173ed51e4710c666b5fbc
DemiTavares908 <59691843+DemiTavares908@users.noreply.github.com>
2020-04-03 15:29:17 +0200

    CSV test file and script

---
+++ Assets/Scripts/CSVScriptReader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CSVScriptReader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextAsset csvdata = Resources.Load<TextAsset>("letter_frequency");
+
+        string[] data = csvdata.text.Split(new char[] { '\n' });
+        for (int o = 0; o < data.Length; o++)
+        {
+            Debug.Log("Value" + o.ToString() + data[2].ToString());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
